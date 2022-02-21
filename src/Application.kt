acdf8bfdde51a3eaa2001f08ecfdfd6d51c927be
Marcelo Santos <rocha.mer21@gmail.com>
2022-02-21 14:07:21 -0300

    iniciando

---
+++ src/Application.kt
@@ -0,0 +1,50 @@
+package com.xyz.marcelo
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.sessions.*
+import io.ktor.auth.*
+import io.ktor.gson.*
+import io.ktor.features.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    install(Sessions) {
+        cookie<MySession>("MY_SESSION") {
+            cookie.extensions["SameSite"] = "lax"
+        }
+    }
+
+    install(Authentication) {
+    }
+
+    install(ContentNegotiation) {
+        gson {
+        }
+    }
+
+    routing {
+        get("/") {
+            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
+        }
+
+        get("/session/increment") {
+            val session = call.sessions.get<MySession>() ?: MySession()
+            call.sessions.set(session.copy(count = session.count + 1))
+            call.respondText("Counter is ${session.count}. Refresh to increment.")
+        }
+
+        get("/json/gson") {
+            call.respond(mapOf("hello" to "world"))
+        }
+    }
+}
+
+data class MySession(val count: Int = 0)
+
